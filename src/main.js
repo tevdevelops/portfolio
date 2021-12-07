59664ee046584efa4e754fdb4a94f737508dbc52
Tevin Rivera <tevdevelops@gmail.com>
2021-12-07 02:27:24 -0500

    Add packages to use font awesome icons. Also add in cdn link for Google font. Add some global styles

--- src/main.js
+++ src/main.js
@@ -5,6 +5,21 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import DefaultLayout from '~/layouts/Default.vue'
 
+// TODO: Refactor this font awesome init into a separate Nuxt.js plugin file
+// Docs -> https://github.com/FortAwesome/vue-fontawesome#nuxtjs
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faGithub,
+  faTwitter,
+  faInstagram,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+config.autoAddCss = false
+library.add(faGithub, faTwitter, faInstagram, faLinkedinIn)
+
 export default function(Vue, { appOptions, head }) {
   head.link.push({
     rel: 'stylesheet',
@@ -18,10 +33,18 @@ export default function(Vue, { appOptions, head }) {
       'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900',
   })
 
+  head.link.push({
+    rel: 'stylesheet',
+    href:
+      'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,600;1,400;1,600;1,900&display=swap',
+  })
+
   const opts = {} //opts includes, vuetify themes, icons, etc.
   Vue.use(Vuetify)
 
   appOptions.vuetify = new Vuetify(opts)
+
+  Vue.component('font-awesome', FontAwesomeIcon)
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
